b37e6588278c102097a959259390d02f9ce0d6d2
elinxer <654753115@qq.com>
2016-12-28 08:59:36 +0800

    zhihu topic

--- file_get_content_proxy.php
+++ file_get_content_proxy.php
@@ -1,6 +1,7 @@
 <?php
 
-$url = 'http://test2.zuzuche.com/_yds_/curl/request.php';
+//$url = 'http://test2.zuzuche.com/_yds_/curl/request.php';
+$url = 'http://w.zuzuche.com/ssp/api/info.php?id=12b4ba28&callback=__showcaseJSONP_1482806958542_3';
 
 
 ini_set("memory_limit", "1024M");
@@ -13,19 +14,17 @@ require dirname(__FILE__).'/phpspider/core/requests.php';
 require dirname(__FILE__).'/phpspider/core/selector.php';
 
 
-$auth =	base64_encode('654753115@qq.com:a654753115');
+//$auth =	base64_encode('654753115@qq.com:a654753115');
 
 $context = array(
-    'http' => array(
+    'https' => array(
         'proxy' => 'tcp://124.88.67.52:843',
         'request_fulluri' => true,
         ),
     );
 $context = stream_context_create($context);
 
-$html    = file_get_contents($url, false, $context);
-
-print_r(json_decode($html, true));
+echo $html    = file_get_contents($url, false, $context);
 
 
 
