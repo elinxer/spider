7826230f693f9bf2d72d97f38f5f8d5f3acb9cd3
elinxer <654753115@qq.com>
2017-01-02 22:21:21 +0800

    zhihu answer

--- zh.php
+++ zh.php
@@ -10,8 +10,6 @@ require dirname(__FILE__).'/phpspider/core/requests.php';
 require dirname(__FILE__).'/phpspider/core/selector.php';
 require dirname(__FILE__).'/phpspider/core/db.php';
 
-
-
 $GLOBALS['config']['db'] = array(
     'host'		=>	'127.0.0.1',
     'port'		=>	3306,
@@ -21,9 +19,42 @@ $GLOBALS['config']['db'] = array(
 );
 
 
+$answer = db::get_one("SELECT 	* FROM 	spider.`zh_question_answer`  LEFT JOIN zh_question_list on zh_question_list.question_code=zh_question_answer.question_code LIMIT 1;");
+$author = db::get_one("SELECT * FROM zhiteer.`user_author` WHERE author_cn_name='{$answer['answer_author_name']}' limit 1");
 
-$result = db::get_one("SELECT * FROM `zh_question_answer` LIMIT 1;");
+if (empty($author))
+{
+    $author = array(
+        'author_cn_name' => $answer['answer_author_name'],
+        'author_en_name' => $answer['answer_author_code'],
+        'author_desc'    => $answer['answer_author_desc'],
+        'author_avater'  => str_replace('_s.jpg', '_xl.jpg', $answer['answer_author_avater']),
+        'author_tags'    => $answer['answer_author_a_desc'],
+    );
+    $author['author_id'] = db::insert('zhiteer.user_author', $author);
+}
 
-echo $html = selector::select($result['answer_html_content'], "//div[contains(@class, 'zm-editable-content')]");
+$html = selector::select($answer['answer_html_content'], "//div[contains(@class, 'zm-editable-content')]");
+$html = str_replace('<i class="icon-external"/>', '', $html);
 
-print_r($result);
+
+if (!empty($html))
+{
+    $article = array(
+        'post_title'  => $answer['question_title'],
+        'post_author' => $author['author_id'],
+        'post_publish_time' => $answer['answer_publish_time'],
+        'post_comefrom' => 'zhihu'
+    );
+    $post_id = db::insert('zhiteer.article_list', $article);
+
+    if (!empty($post_id))
+    {
+        db::insert('zhiteer.article_content', array('post_id' => $post_id,'post_content'=>$html));
+    }
+
+}
+
+
+
+print_r($answer);
