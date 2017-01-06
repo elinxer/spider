115ae319a4c0359156b153a190f71eb544055e1d
elinxer <654753115@qq.com>
2017-01-06 16:49:39 +0800

    zh article

--- file_get_content_proxy.php
+++ file_get_content_proxy.php
@@ -1,5 +1,7 @@
 <?php
 
+require dirname(__FILE__).'/phpspider/core/init.php';
+
 $url = 'http://test2.zuzuche.com/_yds_/curl/request.php';
 // $url = 'http://w.zuzuche.com/ssp/api/info.php?id=12b4ba28&callback=__showcaseJSONP_1482806958542_3';
 
@@ -7,15 +9,22 @@ $url = 'http://test2.zuzuche.com/_yds_/curl/request.php';
 
 $url = 'https://www.zhihu.com//question/19554005';
 
-ini_set("memory_limit", "1024M");
+$GLOBALS['config']['db'] = array(
+    'host'		=>	'127.0.0.1',
+    'port'		=>	3306,
+    'user'		=>	'root',
+    'pass'		=>	'',
+    'name'		=>	'spider',
+);
 
-set_time_limit(600);
+$url = 'https://sp0.baidu.com/9_Q4sjW91Qh3otqbppnN2DJv/pae/channel/data/asyncqury?cb=jQuery110205355966680955171_1483691356756&appid=4001&com=yuantong&nu=123123&vcode=&token=&_=1483691356758&qq-pf-to=pcqq.group';
 
-error_reporting(E_ALL);
+$ip = db::get_one("SELECT * FROM `spider_proxy_ip` WHERE protocol='https' ORDER BY RAND() LIMIT 1;");
+requests::set_proxies(array('http'=>"tcp://{$ip['ip']}:{$ip['port']}"));
+requests::set_useragent(' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36');
+echo requests::get($url);
 
-require dirname(__FILE__).'/phpspider/core/requests.php';
-require dirname(__FILE__).'/phpspider/core/selector.php';
-
+die();
 
 //$auth =	base64_encode('654753115@qq.com:a654753115');
 
