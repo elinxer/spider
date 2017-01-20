95cb61298a22d7a83e2e9261c61d9e90743111c8
elinxer <654753115@qq.com>
2017-01-20 19:00:23 +0800

    zh article

--- spider_keji.php
+++ spider_keji.php
@@ -14,14 +14,43 @@ $GLOBALS['config']['db'] = array(
     'pass'		=>	'',
     'name'		=>	'spider',
 );
-$cat_tbl = "spider.spider_keji_cate";
+$keji_tbl = "spider.spider_keji";
 
 
+$url = 'http://www.paper.edu.cn/releasepaper/subject/11044/常微分方程/2/11044';
+requests::set_useragent(' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36');
 
+$con = requests::get($url);
 
+$con = selector::select($con, "//div[contains(@class, 'r_two')]");
+$con = selector::select($con, "//div[contains(@class, 'cmtdiv')]");
 
+$arr = array();
+if (!empty($con))
+{
+    foreach ($con as $k => $list)
+    {
+        $list = str_replace('&#13;', '', $list);
 
+        $title = selector::select($list, "//p[1]/span/a");
+        $link = selector::select($list, "//p[1]/span/a/@href");
+        $author = selector::select($list, "//p[2]/span[2]");
+        $hash = md5($link);
+        $arr[] = array(
+            'hash' => $hash,
+            'title' => $title,
+            'link' => $link,
+            'author' => $author,
+            'add_time' => time(),
+        );
+    }
+}
 
+echo db::insert_batch($keji_tbl, $arr);
+
+print_r($arr);
+
+die();
 die('===========获取分类=============');
 
 $GLOBALS['config']['db'] = array(
