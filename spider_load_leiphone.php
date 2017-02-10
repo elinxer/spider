d8c35c9cc813a354d2c20a0fa73b924bab89a995
elinxer <654753115@qq.com>
2017-02-10 08:52:31 +0800

    zh article

--- spider_load_leiphone.php
+++ spider_load_leiphone.php
@@ -1,8 +1,17 @@
 <?php
-
+/**
+ * 雷锋网文章下载
+ */
 ini_set("memory_limit", "1024M");
 require dirname(__FILE__).'/phpspider/core/init.php';
 
+$GLOBALS['config']['db'] = array(
+    'host'		=>	'127.0.0.1',
+    'port'		=>	3306,
+    'user'		=>	'root',
+    'pass'		=>	'',
+    'name'		=>	'zhiteer',
+);
 
 //$url = 'http://www.leiphone.com/';
 $url = 'http://www.leiphone.com/site/AjaxLoad/page/1';
@@ -25,6 +34,13 @@ foreach ($arr as $k => $item)
     $title = trim($title);
 
     $link = selector::select($item, "//div[contains(@class, 'word')]/h3/a/@href");
+
+    $hash = md5('leiphone'.$link);
+
+    if (db::get_one("select * from zhiteer.spider_news WHERE hash='{$hash}'"))
+    {
+        continue;
+    }
 
     $img = selector::select($item, "//div[contains(@class, 'img')]/a/img/@data-original");
 
@@ -53,8 +69,8 @@ foreach ($arr as $k => $item)
     $content = selector::select($content_html, "//div[contains(@class, 'article-left')]/div[1]");
     $content = trim($content);
 
-    $news[] = [
-        'hash' => md5('leiphone'.$link),
+    $news = [
+        'hash' => $hash,
         'title' => $title,
         'link'  => $link,
         'cover' => $img,
@@ -69,7 +85,32 @@ foreach ($arr as $k => $item)
         'created_at' => time()
     ];
 
-    break;
+    db::insert("spider_news", $news);
 }
 
-print_r($news);
+/**
+ *
+ * -- 表结构
+
+DROP TABLE IF EXISTS `spider_news`;
+CREATE TABLE `spider_news` (
+`id` int(15) NOT NULL AUTO_INCREMENT,
+`hash` varchar(100) NOT NULL,
+`title` varchar(255) NOT NULL,
+`link` varchar(500) NOT NULL,
+`cover` varchar(500) DEFAULT NULL,
+`img_original` varchar(500) DEFAULT NULL,
+`tags` varchar(255) DEFAULT NULL,
+`author` varchar(255) NOT NULL,
+`author_intro` varchar(255) DEFAULT NULL,
+`author_avater` varchar(500) DEFAULT NULL,
+`content` longtext NOT NULL,
+`intro` varchar(500) DEFAULT NULL,
+`released_at` int(13) NOT NULL,
+`created_at` datetime NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `hash` (`hash`),
+KEY `title` (`title`),
+KEY `author` (`author`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+ */
